13d15c4373045878171e6467779d12310a993e9a
Illya <145700600+MrKopchick@users.noreply.github.com>
2025-04-02 22:01:28 +0300

    add checkRole logic

--- backend/routes/typeRouter.js
+++ backend/routes/typeRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');  
 const router  = Router();  
 const typeController = require('../controllers/typeController');  
+const checkRole = require('../middlewares/checkRoleMiddleware');  
 
-router.post('/', typeController.createType);
+router.post('/', checkRole('ADMIN'), typeController.createType);
 router.get('/', typeController.getAllTypes);
 
 module.exports = router;
