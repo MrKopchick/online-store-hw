d3ba95fd0bee195976ccf97b01711d538a092644
Illya <145700600+MrKopchick@users.noreply.github.com>
2025-04-06 19:55:51 +0300

    fix ErrorHandlingMiddleware.js

--- backend/middleware/ErrorHandlingMiddleware.js
+++ backend/middleware/ErrorHandlingMiddleware.js
@@ -1,9 +1,8 @@
 const ApiError = require('../utils/ApiError');
 
-module.exports = function(err, req, res, next) {
-    if(err instanceof ApiError) {
-        return res.status(err.statusCode).json({ message: err.message });
-    } 
-
-    return res.status(500).json({ message: 'Internal Server Error' });  
+module.exports = function (err, req, res, next) {
+    if (err instanceof ApiError) {
+        return res.status(err.status).json({message: err.message})
+    }
+    return res.status(500).json({message: "Непредвиденная ошибка!"})
 }
